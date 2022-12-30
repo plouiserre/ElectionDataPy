ff7324967464184da575aadb82fae00cfa32da58
Pierre-Louis Serré <plouiserre@gmail.com>
2022-12-30 14:47:50 +0100

    create new database and its tables, initiate party table

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 Get OpenData about 2022 France Elections and insert in a new database
 
 ## Phases
-1. Create workflow structure with sample print
-2. Made Scripts SQL for the structure of the database and initiate the data about political party
+1. Create workflow structure with sample print DID
+2. Made Scripts SQL for the structure of the database DID
 3. From first opendata file store departments
 4. From first opendata file store french districts
 5. From first opendata file store french candidats and their deputies
---
+++ scripts/initDatabase.sql
@@ -0,0 +1,87 @@
+DROP DATABASE IF EXISTS ELECTIONSCONGRESSMANS;
+CREATE DATABASE ELECTIONSCONGRESSMANS;
+
+-- créer l'utilisateur pour la BDD
+DROP USER IF EXISTS 'ElectionsCongressmans'@'localhost'  ;
+CREATE USER 'ElectionsCongressmans'@'localhost' IDENTIFIED BY 'ASimpleP@ssW0rd' ;
+
+-- ajout de droits
+GRANT DELETE ON ELECTIONSCONGRESSMANS.* TO 'ElectionsCongressmans'@'localhost' ;
+GRANT INSERT ON ELECTIONSCONGRESSMANS.* TO 'ElectionsCongressmans'@'localhost' ;
+GRANT UPDATE ON ELECTIONSCONGRESSMANS.* TO 'ElectionsCongressmans'@'localhost' ;
+GRANT SELECT ON ELECTIONSCONGRESSMANS.* TO 'ElectionsCongressmans'@'localhost' ;
+
+USE ELECTIONSCONGRESSMANS;
+
+-- 1 Department
+CREATE TABLE IF NOT EXISTS DEPARTMENT(
+    DepartmentId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    DepartmentName VARCHAR(25)
+);
+
+-- 2 Party
+CREATE TABLE IF NOT EXISTS PARTY(
+    PartyId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    PartyName VARCHAR(50),
+    ShortName VARCHAR(3)
+);
+
+-- 3 District
+CREATE TABLE IF NOT EXISTS DISTRICT(
+    DistrictId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Position INT,
+    DistrictName VARCHAR(25),
+    DepartmentId INT,
+    Registered INT,
+    Abstention INT,
+    Voting INT,
+    BlankVoting INT,
+    NullVoting INT,
+    FOREIGN KEY (DepartmentId) REFERENCES DEPARTMENT(DepartmentId)
+);
+
+-- 4 Candidate
+CREATE TABLE IF NOT EXISTS CANDIDATE(
+    CandidateId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    CandidateLastName VARCHAR(25),
+    CandidateFirstName VARCHAR(25),
+    CandidateSexe VARCHAR(1),
+    CandidateBirthDate DATE,
+    PartyId INT,
+    Job VARCHAR(25),
+    OldCandidate BIT,
+    DistrictId INT,
+    VoteFirstRound INT,
+    VoteSecondRound INT,
+    FOREIGN KEY (PartyId) REFERENCES PARTY(PartyId),
+    FOREIGN KEY (DistrictId) REFERENCES DISTRICT(DistrictId)
+);
+
+-- 5 Deputy
+CREATE TABLE IF NOT EXISTS DEPUTY(
+    DeputyId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    DeputyLastName VARCHAR(25),
+    DeputyFirstName VARCHAR(25),
+    DeputyBirthDate DATE,
+    CandidateId INT,
+    FOREIGN KEY (CandidateId) REFERENCES CANDIDATE(CandidateId)
+);
+
+-- Add Datas 
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers extrême gauche','DXG');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Parti radical de gauche','RDG');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Nouvelle union populaire écologique et sociale','NUP');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers gauche','DVG');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Ecologistes','ECO');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers','DIV');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Régionaliste','REG');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Ensemble ! (Majorité présidentielle)','ENS');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers Centre','DVC');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers','DIV');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Union des Démocrates et des Indépendants','UDI');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Les Républicains','LR');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers droite','DVD');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Droite souverainiste','DSV');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Reconquête !','REC');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Rassemblement National','RN');
+INSERT INTO PARTY(PartyName, ShortName) VALUES ('Divers extrême droite','DXD');

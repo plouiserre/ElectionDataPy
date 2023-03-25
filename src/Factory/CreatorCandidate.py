484b0c5744b756fae4e6ae84f1b9c7b8c3d03e63
Pierre-Louis Serré <pierre-louisserre@pclenovo-1.home>
2023-03-25 15:45:18 +0100

    modify creatorcandidate to have the responsibility to construct candidate model

--- src/Factory/CreatorCandidate.py
+++ src/Factory/CreatorCandidate.py
@@ -1,15 +1,69 @@
+import datetime
+
 from src.Models.CandidateModel import CandidateModel
 from src.Factory.Creator import Creator
 
-#TODO rework this method and this test like CreatorDistrict
+#TODO rework this method and this test like CreatorDistrict 
 class CreatorCandidate(Creator) : 
-    def factory_method(self, candidate_data) :         
-        can = CandidateModel()
-        can.last_name = candidate_data.candidate_last_name
-        can.first_name = candidate_data.candidate_first_name 
-        can.sexe =  candidate_data.candidate_sexe
-        can.birth_date = candidate_data.candidate_birth_date 
-        can.party = candidate_data.candidate_party 
-        can.job = candidate_data.candidate_job 
+    def __init__(self, parties) -> None:
+        self.is_candidate_first_name_simple = True
+        self.datas = []
+        self.can = CandidateModel()
+        self.parties = parties
         
-        return can
+        
+    def factory_method(self, candidate_data) :         
+        self.datas = candidate_data
+        self.can.last_name = self.datas[7]
+        self.can.sexe =  self.datas[6]
+        self.__set_candidate_first_name()
+        self.__set_candidate_birth_date()
+        self.__set_candidate_party()
+        self.__set_candidate_jobs()
+        if self.datas[12] == 'Oui' :
+           self.can.is_sorting = True
+        
+        return self.can
+    
+    def __set_candidate_birth_date(self) : 
+        birthdate = ''
+        if  self.is_candidate_first_name_simple == False :
+            birthdate = self.datas[10]
+        else : 
+            birthdate = self.datas[9]
+        birthdate = birthdate.replace('datetime.datetime(', '')
+        birthdate = birthdate.replace(', 0, 0)','')
+        birthdate = birthdate.replace(')','')
+        birthdate_elements = birthdate.split(',')
+        year = int(birthdate_elements[0])
+        month = int(birthdate_elements[1])
+        day = int(birthdate_elements[2])
+        self.can.birth_date = datetime.datetime(year, month, day)
+    
+    
+    def __set_candidate_first_name(self) :  
+        if str.isalpha(self.datas[9]) : 
+            self.is_candidate_first_name_simple = False
+            self.can.first_name = self.datas[8]+" "+self.datas[9]
+        else :
+            self.can.first_name = self.datas[8]
+            
+            
+    def __set_candidate_party(self) : 
+        party_shortname = ''
+        if  self.is_candidate_first_name_simple :
+           party_shortname = self.datas[10]
+        else :
+           party_shortname = self.datas[11]
+        for party in self.parties : 
+            if party.short_name == party_shortname : 
+                self.can.party_id = party.id
+                break
+            
+            
+    def __set_candidate_jobs(self) : 
+         if  self.is_candidate_first_name_simple :
+            self.can.job = self.datas[11]
+         else :
+            self.can.job = self.datas[12]
+        
--- tests/Factory/test_creator_candidate.py
+++ tests/Factory/test_creator_candidate.py
@@ -1,23 +1,59 @@
 import unittest
+import datetime
 from src.Factory.CreatorCandidate import CreatorCandidate
 
 from tests.helper_test import HelperTest
+from tests.base_unit_test import BaseUnitTest
 
 class CreatorCandidateTest(unittest.TestCase):
     def test_creator_candidate_from_gironde(self) : 
-        creator = CreatorCandidate()
-        helper = HelperTest()
-        candidate_data = helper.get_one_candidate_data_model()
+        creator = self.__get_creator_init()
+        
+        candidate_data = ['XXXXX','XXXXX','XXXXX','XXXXX','XXXXX','XXXXX','M','Cazenave','Thomas','datetime.datetime(1978, 06, 03, 0, 0)','ENS','Cadre de la fonction publique','Non','XXXXX','XXXXX','XXXXX','XXXXX','XXXXX']
         
         candidate = creator.factory_method(candidate_data)
         
-        self.assertEqual("06/03/1978", candidate.birth_date)
-        self.assertEqual("Thomas", candidate.first_name)
-        self.assertEqual("Cazenave", candidate.last_name)
-        self.assertEqual(False, candidate.is_sorting)
-        self.assertEqual("Cadre de la fonction publique", candidate.job)
-        self.assertEqual("ENS", candidate.party)
-        self.assertEqual("M", candidate.sexe)
+        candidate_model_check = ["Cazenave", "Thomas", "M", 8, "Cadre de la fonction publique", datetime.datetime(1978,6,3), False]     
+        self.__assert_candidate_model(candidate_model_check, candidate)
+        
+
+    def test_creator_candidate_woman_sorting_candidate(self) : 
+        creator = self.__get_creator_init()
+        
+        candidate_data = ['XXXXX','XXXXX','XXXXX','XXXXX','XXXXX','XXXXX','F','TRASTOUR-ISNART','Laurence','datetime.datetime(1972, 03, 06, 0, 0)','LR','Cadre de la fonction publique','Oui','XXXXX','XXXXX','XXXXX','XXXXX','XXXXX']
+        
+        candidate = creator.factory_method(candidate_data)
+        
+        candidate_model_check = ["TRASTOUR-ISNART", "Laurence", "F", 12, "Cadre de la fonction publique", datetime.datetime(1972,3,6), True]     
+        self.__assert_candidate_model(candidate_model_check, candidate)
+        
+        
+    def test_creator_candidate_two_first_name(self) : 
+        creator = self.__get_creator_init()
+        
+        candidate_data = ['XXXXX','XXXXX','XXXXX','XXXXX','XXXXX','XXXXX','M','Cazenave','Thomas','Eric','datetime.datetime(1978, 06, 03, 0, 0)','ENS','Cadre de la fonction publique','Non','XXXXX','XXXXX','XXXXX','XXXXX','XXXXX']
+        
+        candidate = creator.factory_method(candidate_data)
+        
+        candidate_model_check = ["Cazenave", "Thomas Eric", "M", 8, "Cadre de la fonction publique", datetime.datetime(1978,6,3), False]     
+        self.__assert_candidate_model(candidate_model_check, candidate)
+        
+    
+    def __get_creator_init(self) : 
+        helper = HelperTest()
+        parties = helper.get_parties()
+        creator = CreatorCandidate(parties)
+        return creator
+    
+    
+    def  __assert_candidate_model(self, candidate_data_check, candidate_model) :
+        self.assertEqual(candidate_data_check[0], candidate_model.last_name)
+        self.assertEqual(candidate_data_check[1], candidate_model.first_name)
+        self.assertEqual(candidate_data_check[2], candidate_model.sexe)
+        self.assertEqual(candidate_data_check[3], candidate_model.party_id)
+        self.assertEqual(candidate_data_check[4], candidate_model.job)
+        self.assertTrue(candidate_data_check[5] == candidate_model.birth_date)
+        self.assertEqual(candidate_data_check[6], candidate_model.is_sorting)
         
 
     if __name__ == "__main__":

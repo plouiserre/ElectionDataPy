0a259d18219c3880deaa93cfd3d9d0b234686665
Pierre-Louis Serré <pierre-louisserre@pclenovo-1.home>
2023-03-26 19:01:56 +0200

    modify creation of deputy entities

--- src/Factory/CreatorDeputy.py
+++ src/Factory/CreatorDeputy.py
@@ -1,14 +1,48 @@
+import datetime
+
 from src.Models.DeputyModel import DeputyModel
 from src.Factory.Creator import Creator
 
 class CreatorDeputy(Creator):
-    def factory_method(self, candidate_data):
-        deputy = DeputyModel()
-        deputy.sexe = candidate_data.deputy.sexe
-        deputy.first_name = candidate_data.deputy.first_name
-        deputy.last_name= candidate_data.deputy.last_name
-        deputy.birth_date = candidate_data.deputy.birth_date
-        return deputy
+    def __init__(self, is_candidate_two_first_name) :
+        self.datas = []
+        self.deputy = DeputyModel()
+        self.is_candidate_two_first_name = is_candidate_two_first_name
+        self.index_modify = 0
+        
+    def factory_method(self, deputy_data):
+        self.datas = deputy_data
+        if self.is_candidate_two_first_name :
+            self.index_modify += 1
+        self.deputy.sexe = self.datas[13 + self.index_modify]
+        self.deputy.last_name = self.datas[14 + self.index_modify]
+        self.__set_deputy_first_name()
+        self.__set_deputy_birthdate()
+        if  self.datas[17 + self.index_modify]  == 'Oui' :
+            self.deputy.is_sorting = True
+        return self.deputy
+    
+    
+    def __set_deputy_first_name(self) : 
+        if ('datetime' in self.datas[15 + self.index_modify + 1]) == False : 
+            self.deputy.first_name = self.datas[15 + self.index_modify] +" "+ self.datas[15 + self.index_modify + 1]
+            self.index_modify += 1
+        else :     
+            self.deputy.first_name = self.datas[15 + self.index_modify]
+            
+       
+    
+    #TODO factorize with birthdate from candidate
+    def __set_deputy_birthdate(self) : 
+        birthdate = self.datas[16 + self.index_modify]
+        birthdate = birthdate.replace('datetime.datetime(', '')
+        birthdate = birthdate.replace(', 0, 0)','')
+        birthdate = birthdate.replace(')','')
+        birthdate_elements = birthdate.split(',')
+        year = int(birthdate_elements[0])
+        month = int(birthdate_elements[1])
+        day = int(birthdate_elements[2])
+        self.deputy.birth_date = datetime.datetime(year, month, day)
     
     
     
--- tests/Factory/test_creator_deputy.py
+++ tests/Factory/test_creator_deputy.py
@@ -5,17 +5,13 @@ from src.Models.DeputyModel import DeputyModel
 from src.Factory.CreatorDeputy import CreatorDeputy
 from helper_test import HelperTest
 
+#TODO factoize self assertpart 
 class CreatorDeputyTest(unittest.TestCase):
     def test_creator_deputy(self):
-        creator = CreatorDeputy()
-        helper = HelperTest()
-        candidate_data = helper.get_one_candidate_data_model()
+        creator = CreatorDeputy(False)
+        deputy_data = ['XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX' ,'XXXXX' , 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'F', 'BARTEBIN-SOURHOU', 'Huguette' ,'datetime.datetime(1971, 12, 3, 0, 0)', 'Non']
         
-        #candidate = creator.factory_method(candidate_data)
-        
-        #candidate_data = ['XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX' ,'XXXXX' , 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'F', 'BARTEBIN-SOURHOU', 'Huguette' ,'datetime.datetime(1971, 12, 3, 0, 0)', 'Non']
-        
-        deputy = creator.factory_method(candidate_data)
+        deputy = creator.factory_method(deputy_data)
         
         self.assertEqual('F', deputy.sexe)
         self.assertEqual('BARTEBIN-SOURHOU', deputy.last_name)
@@ -23,3 +19,56 @@ class CreatorDeputyTest(unittest.TestCase):
         self.assertEqual(datetime.datetime(1971,12,3), deputy.birth_date)
         self.assertFalse(deputy.is_sorting)
         
+        
+    def test_creator_deputy_is_sorting(self):
+        creator = CreatorDeputy(False)
+        deputy_data = ['XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX' ,'XXXXX' , 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'F', 'BARTEBIN-SOURHOU', 'Huguette' ,'datetime.datetime(1971, 12, 3, 0, 0)', 'Oui']
+        
+        deputy = creator.factory_method(deputy_data)
+        
+        self.assertEqual('F', deputy.sexe)
+        self.assertEqual('BARTEBIN-SOURHOU', deputy.last_name)
+        self.assertEqual('Huguette', deputy.first_name)
+        self.assertEqual(datetime.datetime(1971,12,3), deputy.birth_date)
+        self.assertTrue(deputy.is_sorting)
+        
+        
+    def test_creator_deputy_with_candidate_two_first_names(self):
+        creator = CreatorDeputy(True)
+        
+        deputy_data = ['XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'M', 'Cazenave', 'Thomas' ,'Eric', 'datetime.datetime(1978, 06, 03, 0, 0)' , 'ENS', 'Cadre de la fonction publique', 'Non', 'F', 'BARTEBIN-SOURHOU', 'Huguette' ,'datetime.datetime(1971, 12, 3, 0, 0)', 'Non']
+        
+        deputy = creator.factory_method(deputy_data)
+        
+        self.assertEqual('F', deputy.sexe)
+        self.assertEqual('BARTEBIN-SOURHOU', deputy.last_name)
+        self.assertEqual('Huguette', deputy.first_name)
+        self.assertEqual(datetime.datetime(1971,12,3), deputy.birth_date)
+        self.assertFalse(deputy.is_sorting)
+        
+        
+    def test_creator_deputy_with_two_first_names(self) : 
+        creator = CreatorDeputy(False)
+        deputy_data = ['XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX' ,'XXXXX' , 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'F', 'BARTEBIN-SOURHOU', 'Huguette' , 'Georgette', 'datetime.datetime(1971, 12, 3, 0, 0)', 'Oui']
+        
+        deputy = creator.factory_method(deputy_data)
+        
+        self.assertEqual('F', deputy.sexe)
+        self.assertEqual('BARTEBIN-SOURHOU', deputy.last_name)
+        self.assertEqual('Huguette Georgette', deputy.first_name)
+        self.assertEqual(datetime.datetime(1971,12,3), deputy.birth_date)
+        self.assertTrue(deputy.is_sorting)
+        
+        
+    def test_creator_deputy_with_candidate_both_have_two_first_names(self):
+        creator = CreatorDeputy(True)
+        
+        deputy_data = ['XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'XXXXX', 'M', 'Cazenave', 'Thomas' ,'Eric', 'datetime.datetime(1978, 06, 03, 0, 0)' , 'ENS', 'Cadre de la fonction publique', 'Non', 'F', 'BARTEBIN-SOURHOU', 'Huguette' , 'Georgette', 'datetime.datetime(1971, 12, 3, 0, 0)', 'Non']
+        
+        deputy = creator.factory_method(deputy_data)
+        
+        self.assertEqual('F', deputy.sexe)
+        self.assertEqual('BARTEBIN-SOURHOU', deputy.last_name)
+        self.assertEqual('Huguette Georgette', deputy.first_name)
+        self.assertEqual(datetime.datetime(1971,12,3), deputy.birth_date)
+        self.assertFalse(deputy.is_sorting)
